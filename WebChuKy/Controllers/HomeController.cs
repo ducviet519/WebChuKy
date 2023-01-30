e6157f1d3c641574f94ddd4c0ec3b34105e55a1a
DucViet519 <ducviet519@gmail.com>
2023-01-30 14:48:40 +0700

    Add project files.

---
+++ WebChuKy/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebChuKy.Models;
+
+namespace WebChuKy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string id = null)
+        {
+            Signature signature = new Signature()
+            {
+                SignatureID = id,
+            };
+            return View(signature);
+        }
+
+        public JsonResult SendSignature(Signature signature)
+        {
+            string message = String.Empty;
+            string title = String.Empty;
+            string result = String.Empty;
+            try
+            {
+                result = "OK";
+                if (result == "OK")
+                {
+                    message = $"Đã ký thành công";
+                    title = "Thành công!";
+                    result = "success";
+                }
+                else
+                {
+                    message = result;
+                    title = "Lỗi!";
+                    result = "error";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                title = "Lỗi!";
+                result = "error";
+            }
+            return Json(new { Result = result, Title = title, Message = message });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebChuKy/Models/Signature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChuKy.Models
+{
+    public class Signature
+    {
+        public string SignatureID { get; set; }
+        public string SignatureData { get; set; }
+        public string SignatureCode { get; set; }
+    }
+}
